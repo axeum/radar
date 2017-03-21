1ff2d98aa3c5ff81fb927b9e8295605006457b85
gioren <sgrenfurm@gmail.com>
2017-03-21 10:54:38 +0100

    Upgrade to golang 1.8

--- build-image.sh
+++ build-image.sh
@@ -1,7 +1,7 @@
 image_version=$1
 
 echo "Compile and build the application"
-docker run --rm -v $(pwd):/usr/src/myapp -w /usr/src/myapp -e CGO_ENABLED=0 -e GOOS=linux -e GOARCH=amd64 golang:1.6 bash -c "go get -d -v; go build -a --installsuffix cgo -v"
+docker run --rm -v $(pwd):/usr/src/myapp -w /usr/src/myapp -e CGO_ENABLED=0 -e GOOS=linux -e GOARCH=amd64 golang:1.8 bash -c "go get -d -v; go build -a --installsuffix cgo -v -o radar"
 
 echo "Building image radar:$image_version"
 docker build --no-cache=true -t ictu/radar:$image_version .
