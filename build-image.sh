f4b36f2b6be8b573bebca76ac6f047ae0db42c7e
gioren <sgrenfurm@gmail.com>
2017-03-21 10:45:32 +0100

    Fixed build script

--- build-image.sh
+++ build-image.sh
@@ -1,13 +1,13 @@
 image_version=$1
 
-echo "Compile the application"
+echo "Compile and build the application"
 docker run --rm -v $(pwd):/usr/src/myapp -w /usr/src/myapp -e CGO_ENABLED=0 -e GOOS=linux -e GOARCH=amd64 golang:1.6 bash -c "go get -d -v; go build -a --installsuffix cgo -v"
 
-echo "Building radar:$image_version"
+echo "Building image radar:$image_version"
 docker build --no-cache=true -t ictu/radar:$image_version .
 
-echo "Pushing plumber:$image_version and plumber:latest"
-#docker push ictu/radar:$image_version
+echo "Pushing radar:$image_version and radar:latest"
+docker push ictu/radar:$image_version
 
 echo "Cleanup"
 rm -f radar
